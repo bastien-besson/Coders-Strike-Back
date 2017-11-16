ca2603638d922dd35db9338b6edbbc47a08ba86e
Bastien Besson <bastien.besson@gmail.com>
2017-11-16 18:07:35 +0100

    archive

--- CodersStrikeBack/www/game.js
+++ CodersStrikeBack/www/game.js
@@ -1,18 +1,63 @@
-var values = ["3",
-                "5",
-                "12684 7084",
-                "4073 4673",
-                "13010 1927",
-                "6564 7818",
-                "7499 1384",
-                "12549 7565 0 0 -1 1",
-                "12819 6603 0 0 -1 1",
-                "12280 8528 0 0 -1 1",
-                "13088 5640 0 0 -1 1"];
+var ABTesting = 1;
+//var AB = ["3",
+//                "5",
+//                "12684 7084",
+//                "4073 4673",
+//                "13010 1927",
+//                "6564 7818",
+//                "7499 1384",
+//                "12549 7565 0 0 -1 1",
+//                "12819 6603 0 0 -1 1",
+//                "12280 8528 0 0 -1 1",
+//                "13088 5640 0 0 -1 1"];
 
-function readline() {
-    return values.shift();
-}
+//var AB2 = ["3",
+//                "5",
+//                "12684 7084",
+//                "4073 4673",
+//                "13010 1927",
+//                "6564 7818",
+//                "7499 1384",
+//                "12549 7565 0 0 -1 1",
+//                "12819 6603 0 0 -1 1",
+//                "12280 8528 0 0 -1 1",
+//                "13088 5640 0 0 -1 1"];
+
+var values = ["3", // laps
+                "6", // checkpointCount
+                "13096 2295", //checkpointX checkpointY
+                "4584 2156", //checkpointX checkpointY
+                "7333 4939", //checkpointX checkpointY
+                "3348 7252", //checkpointX checkpointY
+                "14595 7674", //checkpointX checkpointY
+                "10583 5031", //checkpointX checkpointY
+                "12549 7565 0 0 -1 1", // podX podY vX vY angle nextCheckPointId
+                "4496 7722 0 0 225 3", // podX podY vX vY angle nextCheckPointId
+                "12280 8528 0 0 -1 1", // podX podY vX vY angle nextCheckPointId
+                "13088 5640 0 0 -1 1"]; // podX podY vX vY angle nextCheckPointId
+
+var values2 = ["3", // laps
+                "6", // checkpointCount
+                "13096 2295", //checkpointX checkpointY
+                "4584 2156", //checkpointX checkpointY
+                "7333 4939", //checkpointX checkpointY
+                "3348 7252", //checkpointX checkpointY
+                "14595 7674", //checkpointX checkpointY
+                "10583 5031", //checkpointX checkpointY
+                "12549 7565 0 0 -1 1", // podX podY vX vY angle nextCheckPointId
+                "4496 7722 0 0 225 3", // podX podY vX vY angle nextCheckPointId
+                "12280 8528 0 0 -1 1", // podX podY vX vY angle nextCheckPointId
+                "13088 5640 0 0 -1 1"]; // podX podY vX vY angle nextCheckPointId
+
+//function readline() {
+//    var val = values.shift();
+
+//    if (values.length == 0) {
+//        values = values2.slice();
+//    }
+//    return val;
+//}
+
 /*********************************** VECTOR ************************************************/
 function Vector(x, y) {
     this.x = x || 0;
@@ -49,11 +94,6 @@ Vector.prototype = {
         var y = this.y - v.y;
         return Math.sqrt(x * x + y * y);
     },
-    distance2: function (v) {
-        var x = this.x - v.x;
-        var y = this.y - v.y;
-        return x * x + y * y;
-    },
     clone: function () {
         return new Vector(this.x, this.y);
     },
@@ -63,30 +103,17 @@ Vector.prototype = {
 }
 
 /*********************************** MOVEMENT ************************************************/
-function Movement(thrust, angle) {
-    this.thrust = thrust || 0;
-    this.angle = angle || 0;
-}
-
-Movement.prototype = {
-    toString: function () {
-        return 'A' + this.angle + ' T' + this.thrust;
-    }
-}
-
 var availableMoves = [
-    new Movement(0, -90),
-    new Movement(0, 90),
-    new Movement(100, 0),
-    new Movement(100, -90),
-    new Movement(100, 90),
-    new Movement(200, -90),
-    new Movement(200, 90),
-    new Movement(200, 0),
+    new Int16Array([0, -90]),
+    new Int16Array([0, 90]),
+    new Int16Array([100, -90]),
+    new Int16Array([100, 90]),
+    new Int16Array([200, -90]),
+    new Int16Array([200, 90]),
+    new Int16Array([200, 0]),
 ];
 
 /********************************** GENETIC ALGORITHM *************************************/
-
 var simulationUnit = function (pod, moves) {
     var that = {
         pod: pod,
@@ -95,37 +122,47 @@ var simulationUnit = function (pod, moves) {
     };
 
     return that;
-}
+};
 
-var simulationGroup = function (pod, minDepth) {
+var simulationGroup = function (minDepth) {
     var that = { group: [] };
     var index = 0;
     var count = Math.pow(availableMoves.length, minDepth);
     var moves = [];
     for (var x = 0; x < count; x++) moves[x] = [];
 
-    that.run = function (pod, moveIndex) {
-        for (i = 0; i < that.group.length; i++) {
-            var currentPod = that.group[i].pod;
+    that.run = function (podIndex, moveIndex) {
+        //var endTimer;
+        for (var i = 0, groupLength = that.group.length; i < groupLength; i++) {
+
+            //endTimer = new Date().getTime();
+            //var time = endTimer - GAME.main.startTimer;
+            //if (time > 120) {
+            //    //printErr('End of run');
+            //    return "error";
+            //}
+
+            var pod = that.group[i].pod;
             var moves = that.group[i].moves;
 
             // Move pod
-            for (j = moveIndex; j < moves.length; j++) {
-                currentPod.move(moves[j]);
-                
-                if (currentPod.position.distance2(GAME.main.checkpoints[currentPod.nextCheckPointId]) < 250000) {
-                    currentPod.checkpointMultiplier++;
+            for (var j = moveIndex, moveLength = moves.length; j < moveLength; j++) {
+                pod.move(moves[j]);
 
-                    if (currentPod.nextCheckPointId == GAME.main.checkpoints.length - 1) {
-                        currentPod.nextCheckPointId = 0;
+                if (pod.position.distance(GAME.main.checkpoints[pod.nextCheckPointId]) < 575) {
+                    pod.checkpointMultiplier++;
+
+                    if (pod.nextCheckPointId == GAME.main.checkpoints.length - 1) {
+                        pod.nextCheckPointId = 0;
                     } else {
-                        currentPod.nextCheckPointId++;
+                        pod.nextCheckPointId++;
                     }
                 }
+
             }
 
             // Calculate weight
-            that.group[i].weight = 20000 * currentPod.checkpointMultiplier - currentPod.position.distance(GAME.main.checkpoints[currentPod.nextCheckPointId]);
+            that.group[i].weight = 100000 * pod.checkpointMultiplier - pod.position.distance(GAME.main.checkpoints[pod.nextCheckPointId]);
         }
     };
 
@@ -141,7 +178,6 @@ var simulationGroup = function (pod, minDepth) {
                       // add new simulation Unit with one more move
                       availableMoves.forEach(move => {
                           var newUnit = simulationUnit(unit.pod.clone(), moves.concat([move]));
-
                           newGroup.push(newUnit);
                       });
                   });
@@ -150,14 +186,22 @@ var simulationGroup = function (pod, minDepth) {
         that.group = newGroup;
     };
 
+    that.update = function (pod) {
+        var index = 0;
+        that.group = [];
+        while (index < count) {
+            that.group.push(simulationUnit(pod.clone(), moves[index]));
+            index++;
+        }
+    };
+
     // Initialize
+    var index = 0;
     while (index < count) {
         for (var i = 0; i < minDepth; i++) {
             var value = Math.floor(index / Math.pow(availableMoves.length, i)) % availableMoves.length;
             moves[index][i] = availableMoves[value];
         }
-
-        that.group.push(simulationUnit(pod.clone(), moves[index]));
         index++;
     }
 
@@ -189,19 +233,19 @@ GAME.Pod.prototype = {
 
         if (isOutput) {
             // Update pod thrust
-            this.thrust = movement.thrust;
+            this.thrust = movement[0];
 
             // Calculate desired destination destination vector
-            this.destination.x = this.position.x + Math.round(Math.cos((this.angle + movement.angle) * GAME.radianCoefficient)) * 100;
-            this.destination.y = this.position.y + Math.round(Math.sin((this.angle + movement.angle) * GAME.radianCoefficient)) * 100;
+            this.destination.x = this.position.x + Math.round(Math.cos((this.angle + movement[1]) * GAME.radianCoefficient)) * 100;
+            this.destination.y = this.position.y + Math.round(Math.sin((this.angle + movement[1]) * GAME.radianCoefficient)) * 100;
         }
 
         // Update pod angle
-        this.angle += movement.angle * GAME.angleCoefficient;
+        this.angle += movement[1] * GAME.angleCoefficient;
 
         // Calculate new speed
-        this.speed.x += Math.cos((this.angle) * GAME.radianCoefficient) * movement.thrust;
-        this.speed.y += Math.sin((this.angle) * GAME.radianCoefficient) * movement.thrust;
+        this.speed.x += Math.cos((this.angle) * GAME.radianCoefficient) * movement[0];
+        this.speed.y += Math.sin((this.angle) * GAME.radianCoefficient) * movement[0];
 
         // Calculate new position
         this.position.x = Math.round(this.position.x + this.speed.x);
@@ -232,7 +276,6 @@ GAME.Pod.prototype = {
     update: function () {
         // 0 : posX; 1 : posY; 2 : speedX; 3 : speedY; 4 : angle; 5 : nextCheckpointId
         var line = readline();
-        GAME.main.logs += line + "\n";
         var inputValues = line.split(' ').map(value => parseInt(value));
 
         // Update turn index for current pod
@@ -249,65 +292,58 @@ GAME.Pod.prototype = {
 
 GAME.main = (function () {
     var that = {
-        playerPods: [],
-        opponentPods: [],
+        playerPods: new Array(2).fill().map(pod => new GAME.Pod()),
+        opponentPods: new Array(2).fill().map(pod => new GAME.Pod()),
         checkpoints: [],
-        isDebug: true,
+        isDebug: false,
         logs: '',
+        startTimer: 0,
     };
 
     // Private variables
     var loopIndex = 1;
-    var initDepth = 5;
-    var finalDepth = 5;
+    var startDepth = 4;
+    var finalDepth = 15;
+    // The genetic algorithm
+    var simulations = simulationGroup(startDepth);
 
     // Initialize game infos
     var initialize = function () {
-        // The pods
-        that.playerPods[0] = new GAME.Pod();
-        that.playerPods[1] = new GAME.Pod();
-        that.opponentPods[0] = new GAME.Pod();
-        that.opponentPods[1] = new GAME.Pod();
         // The number of laps to complete the race.
-        var line = readline();
-        GAME.main.logs += line + "\n";
-        var laps = parseInt(line);
+        var laps = parseInt(readline());
         // The number of checkpoints in the circuit.
-        var line = readline();
-        GAME.main.logs += line + "\n";
-        var checkpointCount = parseInt(line);
+        var checkpointCount = parseInt(readline());
         // Coordinates x and y of each checkpoints
         for (i = 0; i < checkpointCount; i++) {
-            var line = readline();
-            GAME.main.logs += line + "\n";
-            var checkpointCoordinates = line.split(' ');
+            var checkpointCoordinates = readline().split(' ');
             GAME.main.checkpoints.push(new Vector(parseInt(checkpointCoordinates[0]), parseInt(checkpointCoordinates[1])));
         }
     };
 
     // Log all informations for one loop
     var logInfos = function () {
-        //that.logs += "index loop : " + loopIndex + "\n";
-        //that.logs += "POD_01 \n" +
-        //             "       angle: " + that.playerPods[0].angle + " || thrust: " + that.playerPods[0].thrust + "\n" +
-        //             "       turn: " + that.playerPods[0].turnIndex + "||" + " checkpointId: " + that.playerPods[0].nextCheckPointId + "]\n" +
-        //             "       speed: " + that.playerPods[0].speed + " || position: " + that.playerPods[0].position + "\n";
+        that.logs += "index loop : " + loopIndex + "\n";
+        that.logs += "POD_01 \n" +
+                     "       angle: " + that.playerPods[0].angle + " || thrust: " + that.playerPods[0].thrust + "\n" +
+                     "       turn: " + that.playerPods[0].turnIndex + "||" + " checkpointId: " + that.playerPods[0].nextCheckPointId + "]\n" +
+                     "       speed: " + that.playerPods[0].speed + " || position: " + that.playerPods[0].position + "\n";
 
-        //that.logs += "POD_02 \n" +
-        //             "       angle: " + that.playerPods[1].angle + " || thrust: " + that.playerPods[1].thrust + "\n" +
-        //             "       turn: " + that.playerPods[1].turnIndex + "||" + " checkpointId: " + that.playerPods[1].nextCheckPointId + "]\n" +
-        //             "       speed: " + that.playerPods[1].speed + " || position: " + that.playerPods[1].position + "\n";
-        printErr(that.logs);
+        that.logs += "POD_02 \n" +
+                     "       angle: " + that.playerPods[1].angle + " || thrust: " + that.playerPods[1].thrust + "\n" +
+                     "       turn: " + that.playerPods[1].turnIndex + "||" + " checkpointId: " + that.playerPods[1].nextCheckPointId + "]\n" +
+                     "       speed: " + that.playerPods[1].speed + " || position: " + that.playerPods[1].position + "\n";
+        //printErr(that.logs);
         that.logs = "";
     }
 
     that.run = function () {
-        debugger;
         // Read initialization inputs
         initialize();
 
         // Game loop
         while (true) {
+            // Start timer for one turn ( turn is 150ms max. )
+            that.startTimer = new Date().getTime();
 
             // Update pod positions
             that.playerPods.forEach(pod => pod.update());
@@ -315,60 +351,53 @@ GAME.main = (function () {
 
             // Move player's pod with genetic algorithm
             for (var index = 0; index < that.playerPods.length; index++) {
+                var pod = that.playerPods[index];
 
-                if (index == 0) {
-                    // Print movement
-                    print('0 0 50');
+                // Update simulation
+                simulations.update(pod);
 
-                } else {
-                    var pod = that.playerPods[index];
+                // Run simulation
+                simulations.run(index, 0);
 
-                    // Create simulation
-                    var podSimulations = simulationGroup(pod, initDepth);
+                // Increase depth
+                simulations.group = simulations.group.sort((a, b) => b.weight - a.weight) // sort by weight
+                          .slice(0, simulations.group.length / 14) // remove the worst simulations
 
-                    // Run simulation
-                    podSimulations.run(pod, 0);
-
-                    //var indexDepth = initDepth;
-
-                    //podSimulations.group = podSimulations.group.sort((a, b) => b.weight - a.weight) // sort by weight
-                    //                                    .slice(0, podSimulations.group.length / availableMoves.length); // remove the worst simulations
-
-                    //while (indexDepth < finalDepth) {
-                    //    podSimulations.increaseDepth();
-
-                    //    // Run simulation
-                    //    podSimulations.run(pod, indexDepth);
-                    //    indexDepth++;
-                    //}
-
-                    podSimulations.group.sort((a, b) => b.weight - a.weight);
-
-                    // Get best movement
-                    var bestSimulation = podSimulations.group[0];
-
-                    //that.logs += "POD_0" + index + " weight : " + bestSimulation.weight;
-
-                    // Calculate movement
-                    pod.move(bestSimulation.moves[0], true);
-
-                    //// Use BOOST 
-                    if (loopIndex === 1 && index === 0) {
-                        pod.thrust = "BOOST";
-                    }
-
-                    // Print movement
-                    print(pod.destination.x + ' ' + pod.destination.y + ' ' + pod.thrust);
+                var indexDepth = startDepth;
+                while (indexDepth < finalDepth) {
+                    indexDepth++;
+                    simulations.increaseDepth();
+                    simulations.run(index, indexDepth);
                 }
+
+                // Get best movement
+                simulations.group.sort((a, b) => b.weight - a.weight);
+                var bestSimulation = simulations.group[0];
+
+                // Calculate movement
+                pod.move(bestSimulation.moves[0], true);
+
+                //// Use BOOST 
+                if (loopIndex === 1 && index === 0) {
+                    pod.thrust = "BOOST";
+                }
+
+                // Print movement
+                print(pod.destination.x + ' ' + pod.destination.y + ' ' + pod.thrust);
+                loopIndex++;
             }
 
-            // Log informations
-            if (that.isDebug) {
-                logInfos();
-            }
+            //var endTimer = new Date().getTime();
+            //var time = endTimer - GAME.main.startTimer;
+            //printErr("Execution time " + time + "ms \n");
 
-            loopIndex++;
+            //Log informatinos
+            //if (that.isDebug) {
+            //    logInfos();
+            //}
+
         }
+
     };
 
     return that;
